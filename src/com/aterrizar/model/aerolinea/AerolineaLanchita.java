824bd7739cce518eccc46e8102b6ba3e21dc9f52
goncxjo <gonzalo.moreira@live.com.ar>
2019-06-07 11:06:47 -0300

    es interfaz

--- src/com/aterrizar/model/aerolinea/AerolineaLanchita.java
+++ src/com/aterrizar/model/aerolinea/AerolineaLanchita.java
@@ -3,7 +3,7 @@ package com.aterrizar.model.aerolinea;
 import com.aterrizar.exception.AsientoLanchitaNoDisponibleException;
 import java.util.List;
 
-public abstract class AerolineaLanchita {
-    public abstract List<List<String>> asientosDisponibles(String origen, String fechaSalida, String destino, String fechaLlegada);
-    public abstract void comprar(String codigoAsiento) throws AsientoLanchitaNoDisponibleException;
+public interface AerolineaLanchita {
+    List<List<String>> asientosDisponibles(String origen, String fechaSalida, String destino, String fechaLlegada);
+    void comprar(String codigoAsiento) throws AsientoLanchitaNoDisponibleException;
 }
