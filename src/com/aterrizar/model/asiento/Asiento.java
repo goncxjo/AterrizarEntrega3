5cdd2a9d60b56013568502142bde925876bfce25
goncxjo <gonzalo.moreira@live.com.ar>
2019-06-07 09:06:40 -0300

    corrección alcance de esSuperOferta

--- src/com/aterrizar/model/asiento/Asiento.java
+++ src/com/aterrizar/model/asiento/Asiento.java
@@ -8,7 +8,7 @@ public abstract class Asiento {
     protected double precio;
     protected Ubicacion ubicacion;
     protected Estado estado;
-    private boolean esSuperOferta = false;
+    protected boolean esSuperOferta = false;
 
     public Asiento() {}
 
