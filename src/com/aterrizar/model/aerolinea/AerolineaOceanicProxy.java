fa68faa97d38a1b190d8cdfeb7b13d352945f270
usueldo <usueldo@gmail.com>
2019-06-08 18:20:50 -0300

    esqueleto de busqueda para oceanic

--- src/com/aterrizar/model/aerolinea/AerolineaOceanicProxy.java
+++ src/com/aterrizar/model/aerolinea/AerolineaOceanicProxy.java
@@ -1,7 +1,11 @@
 package com.aterrizar.model.aerolinea;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.aterrizar.exception.AsientoNoDisponibleException;
 import com.aterrizar.exception.ParametroVacioException;
+import com.aterrizar.model.asiento.AsientoDTO;
 import com.aterrizar.model.usuario.Usuario;
 import com.aterrizar.model.vueloasiento.VueloAsientoFiltro;
 
@@ -15,10 +19,36 @@ public class AerolineaOceanicProxy extends Aerolinea {
 
     @Override
     public Aerolinea filtrarAsientos(VueloAsientoFiltro filtro, Usuario usuario) throws ParametroVacioException {
-        return null;
+    	List<AsientoDTO> asientosDisponibles = new ArrayList<AsientoDTO>();
+    	validarParametros(filtro);
+        usuario.agregarFiltroAlHistorial(filtro);
+        
+        
+       /* if(filtro.getDestino() == null) {
+            asientosDisponibles = this.aerolineaOceanic.asientosDisponiblesParaOrigen(
+                    filtro.getOrigen().name()
+                    , filtro.getFecha()
+            );*/
+        	
+        //}else{
+
+        	asientosDisponibles = this.aerolineaOceanic.
+        							   asientosDisponiblesParaOrigenYDestino
+        							   	(filtro.getOrigen().toString(), 
+        							   	 filtro.getFecha(),
+        							   	 filtro.getDestino().toString());
+        //}
+        
+
+        if(!asientosDisponibles.isEmpty()) {
+           // mapearAsientos(filtro, usuario, asientosDisponibles);
+        }
+
+        return this;
     }
 
-    @Override
+
+	@Override
     public void comprar(String codigoAsiento, Usuario usuario) throws AsientoNoDisponibleException {
 
     }
