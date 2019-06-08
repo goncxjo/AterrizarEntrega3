4f6798073c1cb19f6fc0fd76dc9de5eddf529194
usueldo <usueldo@gmail.com>
2019-06-08 19:26:06 -0300

    se realizan correcciones de tefi con AerolineaOceanicProxyTest

--- test/com/aterrizar/model/aerolinea/AerolineaOceanicProxyTest.java
+++ test/com/aterrizar/model/aerolinea/AerolineaOceanicProxyTest.java
@@ -12,6 +12,9 @@ import com.aterrizar.model.asiento.Ejecutivo;
 import com.aterrizar.model.asiento.PrimeraClase;
 import com.aterrizar.model.asiento.Turista;
 import com.aterrizar.util.date.DateHelper;
+
+import junit.framework.Assert;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -27,103 +30,69 @@ public class AerolineaOceanicProxyTest {
     public void setUp() throws Exception {
 
         MockitoAnnotations.initMocks(this);
-
-        //Asientos disponibles con vuelos desde Buenos Aires
-        when(mockOceanic.asientosDisponiblesParaOrigen("BUE", "31/12/1990"))
-                .thenReturn(this.generarAsientosOrigen());
-
-        //Asientos disponibles con vuelos desde Buenos Aires a Mexico
-        when(mockOceanic.asientosDisponiblesParaOrigenYDestino("BUE", "31/12/1990","MEX"))
-                .thenReturn(this.generarAsientosDeBUEaMEX());
-
-        //Asientos disponibles con vuelos desde Buenos Aires a Los Angeles
-        when(mockOceanic.asientosDisponiblesParaOrigenYDestino("BUE", "31/12/1990","SLA"))
-                .thenReturn(this.generarAsientosDeBUEaSLA());
-
-
-        //Se indican todos los asientos reservados
-        when(mockOceanic.estaReservado("OCE 010", 1))
-                .thenReturn(true);
-
-        when(mockOceanic.estaReservado("OCE 004", 2))
-                .thenReturn(true);
-
-        when(mockOceanic.estaReservado("OCE 001", 7))
-                .thenReturn(true);
-
-
-        //Se indican asientos que se pueden comprar si no se compro nunca antes
-        when(mockOceanic.comprarSiHayDisponibilidad(anyString(), eq("OCE 001"), eq(1)))
-                .thenReturn(true);
-
-        when(mockOceanic.comprarSiHayDisponibilidad(anyString(), eq("OCE 002"), eq(1)))
-                .thenReturn(true);
-
-        when(mockOceanic.comprarSiHayDisponibilidad(anyString(), eq("OCE 003"), eq(1)))
-                .thenReturn(true);
-
-        when(mockOceanic.comprarSiHayDisponibilidad(anyString(), eq("OCE 004"), eq(1)))
-                .thenReturn(true);
-
-        //Se indican asientos que se pueden reservar si no se reservo nunca antes
-        when(mockOceanic.reservar(anyString(), eq("OCE 001"), eq(1)))
-                .thenReturn(true);
-
-        when(mockOceanic.reservar(anyString(), eq("OCE 002"), eq(1)))
-                .thenReturn(true);
-
-        when(mockOceanic.reservar(anyString(), eq("OCE 003"), eq(1)))
-                .thenReturn(true);
-
-        when(mockOceanic.reservar(anyString(), eq("OCE 004"), eq(1)))
-                .thenReturn(true);
-    }
+       }
 
 
     @Test
     public void asientosDisponiblesParaOrigen_ObtenerTodosAsientosDesdeBUE() {
+
+        //Asientos disponibles con vuelos desde Buenos Aires
+        when(mockOceanic.asientosDisponiblesParaOrigen("BUE", "31/12/1990"))
+                .thenReturn(this.generarAsientosOrigen());
+        
         List<AsientoDTO> asientosOrigen = mockOceanic.asientosDisponiblesParaOrigen("BUE", "31/12/1990");
+        
         //Todos los asientos disponibles para Buenos Aires
-        System.out.println("Todos los asientos disponibles para Buenos Aires:");
-        for (AsientoDTO asiento: asientosOrigen) {
-            System.out.println(asiento.getAsiento());
-        }
+        Assert.assertTrue("No contiene 4 vuelos", asientosOrigen.size() == 4);
     }
 
     @Test
     public void asientosDisponiblesParaOrigenYDestino_ObtenerAsientosDesdeBUEaMEX() {
-        List<AsientoDTO> asientos = mockOceanic.asientosDisponiblesParaOrigenYDestino("BUE", "31/12/1990","MEX");
-        //Todos los asientos disponibles desde Buenos Aires a Mexico
-        System.out.println("Todos los asientos disponibles desde Buenos Aires a Mexico:");
-        for (AsientoDTO asiento: asientos) {
-            System.out.println(asiento.getAsiento());
-        }
+        //Asientos disponibles con vuelos desde Buenos Aires a Mexico
+        when(mockOceanic.asientosDisponiblesParaOrigenYDestino("BUE", "31/12/1990","MEX"))
+                .thenReturn(this.generarAsientosDeBUEaMEX());
+        
+    	List<AsientoDTO> asientos = mockOceanic.asientosDisponiblesParaOrigenYDestino("BUE", "31/12/1990","MEX");
+        
+       //Todos los asientos disponibles desde Buenos Aires a Mexico
+        Assert.assertTrue("No contiene 2 vuelos", asientos.size() == 2);
     }
 
     @Test
     public void asientosDisponiblesParaOrigenYDestino_ObtenerAsientosDesdeBUEaSLA() {
-        List<AsientoDTO> asientos = mockOceanic.asientosDisponiblesParaOrigenYDestino("BUE", "31/12/1990","SLA");
+
+        //Asientos disponibles con vuelos desde Buenos Aires a Los Angeles
+        when(mockOceanic.asientosDisponiblesParaOrigenYDestino("BUE", "31/12/1990","SLA"))
+                .thenReturn(this.generarAsientosDeBUEaSLA());
+        
+    	List<AsientoDTO> asientos = mockOceanic.asientosDisponiblesParaOrigenYDestino("BUE", "31/12/1990","SLA");
         //Todos los asientos disponibles desde Buenos Aires a Mexico
-        System.out.println("Todos los asientos disponibles desde Buenos Aires a Los Angeles:");
-        for (AsientoDTO asiento: asientos) {
-            System.out.println(asiento.getAsiento());
-        }
+        Assert.assertTrue("No contiene 2 vuelos", asientos.size() == 2);
     }
 
     @Test
     public void estaReservado_asientoNoEstaReservado() {
+
         //El asiento OCE 001 1 esta disponible
         assertFalse("El asiento OCE 001 01 esta reservado",mockOceanic.estaReservado("OCE 001", 1));
     }
 
     @Test
     public void estaReservado_asientoEstaReservado() {
+
+        when(mockOceanic.estaReservado("OCE 010", 1))
+        .thenReturn(true);
+        
         //El asiento OCE 010 1 esta reservado
         assertTrue("El asiento OCE 010 01 no esta reservado",mockOceanic.estaReservado("OCE 010", 1));
     }
 
     @Test
     public void comprarSiHayDisponibilidad_SeCompraAsientoDisponible() {
+
+        when(mockOceanic.comprarSiHayDisponibilidad(anyString(), eq("OCE 001"), eq(1)))
+        .thenReturn(true);
+        
         //El asiento OCE 001 1 se pudo comprar
         assertTrue("El asiento OCE 001 1 no se pudo comprar",
                 mockOceanic.comprarSiHayDisponibilidad("40854236", "OCE 001", 1));
@@ -138,6 +107,9 @@ public class AerolineaOceanicProxyTest {
 
     @Test
     public void reservar_SeReservaAsientoDisponible() {
+        when(mockOceanic.reservar(anyString(), eq("OCE 001"), eq(1)))
+        .thenReturn(true);
+        
         //El asiento OCE 001 1 se pudo reservar
         assertTrue("El asiento OCE 001 1 no se pudo reservar",
                 mockOceanic.reservar("40854236", "OCE 001", 1));
